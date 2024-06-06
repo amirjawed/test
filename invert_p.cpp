8f10bd139e80226f2cc27b8b653ec8e7b55ec1b2
Amir Jawed <amirimteyaz1@gmail.com>
2024-06-06 14:47:43 +0530

    write code for inverted pyramid

---
+++ invert_p.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int col,row;
+    int n;
+    cin>>n;
+    for(row=0;row<n;row=row+1)
+    {
+         for(col=0;col<n-row;col=col+1){
+            
+                cout<<"* ";
+                }
+                cout<<endl;
+                
+            }
+            }
